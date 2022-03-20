2e8977daa7ff1b700b7e5ec858be1bdf61e14224
shylag1073 <shylag1073@gmail.com>
2022-03-20 12:27:13 -0500

    finishing questions

--- employee.js
+++ employee.js
@@ -1,5 +1,5 @@
 const inquirer = require("inquirer");
-const mysql =("mysql")
+const mysql =("mysql2");
 
 // beginning of questions
 inquirer
@@ -84,7 +84,7 @@ inquirer
                 })
             }
 
-        })    
+        })    //Viewing stuff// 
     } else if (answer.Options === "view") {
         inquirer
         .prompt({
@@ -92,9 +92,122 @@ inquirer
           message: "What do you want to VIEW?",
           name: "View",
           choices: ["department", "role", "employee"]
+        })
+        .then(function(answer){
+            console.log(answer);
+            // viewing departments
+            if (answer.View === "department") {
+                connection.query(
+                    "SELECT * FROM department",
+                    {name: answer.View},
+                    function(error,result,fields) {
+                        if (error) throw error;
+                        console.log(result);
+                    }
+                );
+            }
+            //view roles
+             else if (answer.View === "role") {
+                connection.query(
+                    "SELECT * FROM role",
+                    {name: answer.View},
+                    function(error,result,fields) {
+                        if (error) throw error;
+                        console.log(result);
+                    }
+                );
+            }
+            // view employee
+            else if (answer.View === "employee") {
+                connection.query(
+                    "SELECT * FROM employee",
+                    {name: answer.View},
+                    function(error,result,fields) {
+                        if (error) throw error;
+                        console.log(result);
+                    }
+                );
+            }
         });
-        
+        /// Removing  stuff start 
+ } else if (answer.Options === "remove"){
+     inquirer
+     .prompt({
+         type:"list",
+         message:"Where do you want to remove from?",
+         name:"Remove",
+         choices:["department","role","employee"]
+     })
+     .then(function(answer){
+         console.log(answer);
+         // remove department 
+         if (answer.Remove === "department") {
+             inquirer
+             .prompt({
+                 type:"input",
+                 message:"What is the name of the department you would like to remove?",
+                 name:"removeDep"
+             })
+             .then(function(answer){
+                 console.log(answer);
+                 connection.connect();
+
+                 connection.query(
+                     "DELETE FROM department WHERE ?",
+                     {name:answer.removeDep},
+                     function(error,results,field) {
+                         if (error) throw error;
+                         console.log(results);
+                     }
+                 );
+             });
+         }
+      //// remove role start
+     else if (answer.Remove === "role") {
+        inquirer
+        .prompt({
+            type:"input",
+            message:"What is the name of the role you would like to remove?",
+            name:"removeRole"
+        })
+        .then(function(answer){
+            console.log(answer);
+            connection.connect();
+
+            connection.query(
+                "DELETE FROM role WHERE ?",
+                {name:answer.removeRole},
+                function(error,results,field) {
+                    if (error) throw error;
+                    console.log(results);
+                }
+            );
+        });
+    }
+    /// remove employeee 
+     else if (answer.Remove === "employee") {
+        inquirer
+        .prompt({
+            type:"input",
+            message:"What is the name of the employee you would like to remove?",
+            name:"removeEmploy"
+        })
+        .then(function(answer){
+            console.log(answer);
+            connection.connect();
+
+            connection.query(
+                "DELETE FROM department WHERE ?",
+                {name:answer.removeEmploy},
+                function(error,results,field) {
+                    if (error) throw error;
+                    console.log(results);
+                }
+            );
+        });
     }
 
-   
-})
+     });
+ 
+    } 
+});
